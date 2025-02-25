466f8eea57c828398d4bd7c4c172a04da1082ba8
Bompally Harish <harishfebblar@gmail.com>
2025-02-25 12:34:44 +0530

    chapter 2- created basic component for button

--- reactPracticeByPrashanth/src/App.jsx
+++ reactPracticeByPrashanth/src/App.jsx
@@ -1,6 +1,13 @@
+import KgButton from "./KgButton";
+
 
 function App() {
-  return <h1>This is my first react application</h1>
+
+  return <div>
+    <h1>This is my first react application</h1>
+    <KgButton></KgButton>
+  </div>
+
 }
 
 export default App;
