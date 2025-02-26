d297b8b7dd34b61201a3e180d61fc391e332b911
Bompally Harish <harishfebblar@gmail.com>
2025-02-26 12:20:55 +0530

    first method of conditional statement

--- 18Fragments/src/App.jsx
+++ 18Fragments/src/App.jsx
@@ -2,11 +2,23 @@
 import './App.css'
 import React from 'react'
 
+let healthyItems = ['Amla', 'Apple', 'carrot', 'beetroot']
+// healthyItems = []
+
 function App() {
 
+  if (healthyItems.length == 0) {
+    return <h1>Healthy Items not available</h1>
+
+  }
 
   return <>
     <h1>Healthy Food</h1>
+    <ul class="list-group">
+      {healthyItems.map((item, index) => (
+        <li class="list-group-item" key={index}>{item}</li>
+      ))}
+    </ul>
   </>
 
 
