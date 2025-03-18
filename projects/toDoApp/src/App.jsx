9e216ef9f53ed3c72cbf87996a3ffac1f3b21b66
Bompally Harish <harishfebblar@gmail.com>
2025-03-18 13:16:00 +0530

    chapter 36 use reducer changes

--- projects/toDoApp/src/App.jsx
+++ projects/toDoApp/src/App.jsx
@@ -2,10 +2,24 @@ import AppName from "./components/AppName"
 import AddToDoItem from "./components/AddToDoItem"
 import ItemsList from "./components/itemsList"
 import "./App.css"
-import { useState } from "react"
+import { useReducer, useState } from "react"
 import { ToDoItemsContext } from "./stores/ToDoItemsStore"
 
+const toDoItemsReducer = (currItems, action) => {
+  let newToDoItems = currItems
+  if (action.type === 'NEW_ITEM') {
+    newToDoItems = [
+      ...currItems,
+      { toDo: action.payload.toDo, dueDate: action.payload.dueDate }
+    ]
+  } else if (action.type === 'DELETE_ITEM') {
+    newToDoItems = currItems.filter((x) => x.toDo != action.payload.toDo)
+  }
+  return newToDoItems
+}
+
 function App() {
+
   const defaultItems = [
     {
       toDo: "Buy Fruits",
@@ -16,20 +30,38 @@ function App() {
       dueDate: "18-03-2025"
     }
   ]
-  const [toDoItems, setToDoItems] = useState(defaultItems)
+
+  // const [toDoItems, setToDoItems] = useState(defaultItems)
+
+  const [toDoItems, dispatchItems] = useReducer(toDoItemsReducer, defaultItems)
 
 
   const addItem = (item, dueDate) => {
     if (item != "" && dueDate != "") {
       // const newToDoItems = [...toDoItems, { toDo: item, dueDate }]
       // setToDoItems(newToDoItems)
-      setToDoItems((currValue) => [...currValue, { toDo: item, dueDate }])
+      // setToDoItems((currValue) => [...currValue, { toDo: item, dueDate }])
+      const newActionItem = {
+        type: 'NEW_ITEM',
+        payload: {
+          toDo: item,
+          dueDate
+        }
+      }
+      dispatchItems(newActionItem)
     }
   }
 
   const deleteItem = (item) => {
-    const newToDoItems = toDoItems.filter((x) => x.toDo != item)
-    setToDoItems(newToDoItems)
+    // const newToDoItems = toDoItems.filter((x) => x.toDo != item)
+    // setToDoItems(newToDoItems)
+    const newActionItem = {
+      type: 'DELETE_ITEM',
+      payload: {
+        toDo: item
+      }
+    }
+    dispatchItems(newActionItem)
   }
 
 
